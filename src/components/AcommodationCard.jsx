8b4c6a9d31ae69b4d5b00f78f9b1fdc700126cae
Ankit Gope <ankitgope21@gmail.com>
2025-05-07 18:10:15 +0530

    updated the acoomodation

--- src/components/AcommodationCard.jsx
+++ src/components/AcommodationCard.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
-import acommodationImage from "../assets/images/image.svg"
 import { GiConfirmed } from "react-icons/gi";
+import { RxCountdownTimer } from "react-icons/rx";
 
-const AcommodationCard = () => {
+const AcommodationCard = ({image,heading,checkIn,checkOut,confirm}) => {
   return (
     <div className="min-w-[250px] mt-5 ml-5 rounded-xl bg-[#4D4D4D]  text-white ">
-      <img src={acommodationImage} className="rounded-lg mx-0 mt-0 w-full" />
+      <img src={image} className="rounded-lg mx-0 mt-0 w-full max-h-[150px] object-cover" />
       <div className='flex flex-col items-start justify-items-start ml-4 mt-5'>
-      <h2 className="font-Poppins font-semibold text-[14px]">Shinagawa Prince Hotel</h2>
-      <p className='font-extralight text-[12px]'><span className="font-Poppins font-semibold text-[14px]">Check in:</span> 26.01.2025, 11:15 pm</p>
-      <p className='font-extralight text-[12px]'><span className="font-Poppins font-semibold text-[14px]">Check out:</span> 28.01.2025, 11:15 am</p>
+      <h2 className="font-Poppins font-semibold text-[14px]">{heading}</h2>
+      <p className='font-extralight text-[12px]'><span className="font-Poppins font-semibold text-[14px]">Check in:</span> {checkIn}</p>
+      <p className='font-extralight text-[12px]'><span className="font-Poppins font-semibold text-[14px]">Check out:</span> {checkOut}</p>
 
       </div>
       <div className='mt-3 pb-2 flex justify-between items-center mx-3'>
         <p className='font-Poppins text-[12px]'> 2 Nights</p>
-      <p className="font-medium text-[12px] flex items-center text-[#90EB61] gap-1"><GiConfirmed />Confirmed</p>
+        {
+            confirm ?<p className="font-medium text-[12px] flex items-center text-[#90EB61] gap-1"><GiConfirmed />Confirmed</p>:<p className="font-medium text-[12px] flex items-center text-[#FF6525] gap-1"><RxCountdownTimer />Pending</p>
+        }
+      
       </div>
     </div>
   )
--- src/components/AcommodationSection.jsx
+++ src/components/AcommodationSection.jsx
@@ -1,17 +1,33 @@
 import React from 'react'
+import { useState } from 'react'
 import AcommodationCard from './AcommodationCard'
+import acommodationImage from "../assets/images/image.svg"
+import accomodationImage2 from "../assets/images/image2.svg"
 
 const AcommodationSection = () => {
-  return (
-    <div className='overflow-x-auto scrollbar-hide'>
-        <div className="flex ">
-        <AcommodationCard />
-        <AcommodationCard />
-        <AcommodationCard />
+    const [images, setImages] = useState()
+
+    return (
+        <div className='overflow-x-auto scrollbar-hide'>
+            <div className="flex ">
+                <AcommodationCard image={acommodationImage} heading={"Shinagawa Prince Hotel"} checkIn={"26.01.2025, 11:15 pm"} checkOut={"28.01.2025, 11:15 am"} confirm={true} />
+
+
+                <AcommodationCard image={accomodationImage2}
+                    heading={"Mercure Tokyo Hotel"} checkIn={"28.01.2025, 6:00 pm"} checkOut={"30.01.2025, 11:15 am"} confirm={false} />
+
+
+                <AcommodationCard image={acommodationImage} heading={"Shinagawa Prince Hotel"} checkIn={"26.01.2025, 11:15 pm"} checkOut={"28.01.2025, 11:15 am"} confirm={true} />
+
+
+                <AcommodationCard image={accomodationImage2}
+                    heading={"Mercure Tokyo Hotel"} checkIn={"28.01.2025, 6:00 pm"} checkOut={"30.01.2025, 11:15 am"} confirm={false} />
+
+
+            </div>
+
         </div>
-        
-    </div>
-  )
+    )
 }
 
 export default AcommodationSection
